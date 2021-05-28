af0128fb3691817ebdf07ce3f1eaea62bfe1f11e
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-05-28 12:32:59 -0500

    :sparkles: Refact and adding components

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <header>Aqui va el Header</header>;
+
+export default Header;
---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+const App = () => (
+  <div className='App'>
+    <Header></Header>
+    {/* <Footer></Footer> */}
+  </div>
+);
+
+export default App;
