4b3e8f6603b77c1ffa8db6a2ae8cdb0ff9d3f19d
2Andres3 <aenarvaez14@misena.edu.co>
2021-07-23 20:32:41 -0500

    fix footer

--- src/components/Footer_home.jsx
+++ src/components/Footer_home.jsx
@@ -3,21 +3,18 @@ import { Link } from 'react-router-dom';
 import logo1 from '../assets/img/R-Green_2.0.svg';
 
 const Footer_home = () => (
-    <section id='footer_home' className='bottom-0 w-full h-28'>
+    <section id='footer_home' className='bottom-0 w-full h-20'>
 
         <div className='flex items-stretch'>
 
-            <div className='flex pl-36  inline-block items-center justify-center'>
+            <div className='flex w-1/2 pt-6 pl-36  inline-block items-center justify-center'>
                 <img src={logo1} className='w-48' />
                 <br />
-                <p>© 2021 R-Green Company</p>
             </div>
-
-            <div className='flex w-1/3 sm:w-1/3 items-center justify-center'>
+            <div className='flex w-1/3 sm:w-1/3 pt-4 items-center justify-center'>
                 <hr />
             </div>
-
-            <div className='flex mt-8 w-1/3 sm:w-1/3 inline-block items-center justify-center'>
+            <div className='flex mt-8 w-1/2 pr-28  sm:w-1/2 inline-block items-center justify-center'>
                 <div>
                     <Link to='/' style={{ marginRight: '1em' }} >About Us</Link>
                     
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import Footer from './Footer';
+import Footer from './Footer_home';
 
 const Layout = ({ children }) => (
   <div className='App'>
