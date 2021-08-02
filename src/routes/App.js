3727fb1650e2859e499efafadc27b30e41453b3e
Ederson Fajardo <75399065+EdersonFajardo@users.noreply.github.com>
2021-08-02 02:47:09 -0500

    Forget Password
    Estructura para recuperar contraseña

--- src/routes/App.js
+++ src/routes/App.js
@@ -8,6 +8,7 @@ import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
 import Porfile from '../components/Porfile';
 import Order from '../containers/Order';
+import ForgetPassword from '../containers/ForgetPassword';
 
 
 const App = () => (
@@ -17,6 +18,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/forgetPassword' component={ForgetPassword} />
         <Route exact path='/request' component={Request} />
         <Route exact path='/porfile' component={Porfile} />
         <Route exact path='/order' component={Order}/>
